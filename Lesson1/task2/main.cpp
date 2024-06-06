cf765e0d3ac075c3f4cb7e71aecd2b52ab9b1720
Taras Yarovets <taras.yarovets@gmail.com>
2024-06-06 16:07:42 +0300

    Added 2 task from 1 lesson

---
+++ Lesson1/task2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float firstnumber, secondnumber, sum;
+    cout << "Insert two nubers: " << endl;
+    cout << "Insert first number: " << endl;
+    cin >> firstnumber;
+    cout << "Insert second number: " << endl;
+    cin >> secondnumber;
+    sum = firstnumber + secondnumber;
+    cout << "This your number: " << sum << endl;
+    return 0;
+}
